c5b30980c37e895317258ab750d981929483d997
Rick South <rick@natamus.com>
2024-02-29 11:49:53 +0100

    What Durability source code for 1.20.1.

---
+++ Common/src/main/java/com/natamus/whatdurability/mixin/ItemStackMixin.java
@@ -0,0 +1,32 @@
+package com.natamus.whatdurability.mixin;
+
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = ItemStack.class, priority = 1001)
+public class ItemStackMixin {
+	@Inject(method = "isDamageableItem()Z", at = @At(value = "HEAD"), cancellable = true)
+	public void isDamageableItem(CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(false);
+	}
+
+	@Inject(method = "isDamaged()Z", at = @At(value = "HEAD"), cancellable = true)
+	public void isDamaged(CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(false);
+	}
+
+	@Inject(method = "getDamageValue()I", at = @At(value = "HEAD"), cancellable = true)
+	public void getDamageValue(CallbackInfoReturnable<Integer> cir) {
+		cir.setReturnValue(0);
+	}
+
+	@Inject(method = "setDamageValue(I)V", at = @At(value = "HEAD"))
+	public void setDamageValue(int damage, CallbackInfo ci) {
+		ItemStack itemStack = (ItemStack)(Object)this;
+		itemStack.getOrCreateTag().putInt("Damage", 0);
+	}
+}
---
+++ Common/src/main/java/com/natamus/whatdurability/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.whatdurability.util;
+
+public class Reference {
+	public static final String MOD_ID = "whatdurability";
+	public static final String NAME = "What Durability?";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
+}
