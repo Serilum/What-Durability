7264742cfae1b45930ffeb2fe1c064b6d212e898
Rick South <rick@natamus.com>
2024-08-15 16:04:09 +0200

    (v1.2) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/whatdurability/util/Reference.java
+++ Common/src/main/java/com/natamus/whatdurability/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.whatdurability.util;
 public class Reference {
 	public static final String MOD_ID = "whatdurability";
 	public static final String NAME = "What Durability?";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
