bed15dc1edbff8688e82304ec7653cad806da15c
Rick South <rick@natamus.com>
2026-06-18 12:34:12 +0200

    What Durability source code for 26.2.0.

--- Common/src/main/java/com/natamus/whatdurability/util/Reference.java
+++ Common/src/main/java/com/natamus/whatdurability/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "whatdurability";
 	public static final String NAME = "What Durability?";
 	public static final String VERSION = "1.3";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
